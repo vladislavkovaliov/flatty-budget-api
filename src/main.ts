cc0f4b04fcaa960b82fe2d5d58fcc91e2a54b073
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2025-02-11 15:11:10 +0300

    feat: adds handling malicious request data

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,12 @@ async function bootstrap() {
 
     app.use(morgan("combined"));
 
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
 
     await app.listen(process.env.PORT ?? 3000);
 }
