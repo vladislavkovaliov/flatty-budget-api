20212a56b675ca1d1284774981d8c32d8842f7df
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2025-02-14 19:18:03 +0300

    feat: adds get endpoint to request locations

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from "@nestjs/common";
+import {
+    Controller,
+    DefaultValuePipe,
+    Get,
+    ParseIntPipe,
+    Query,
+} from "@nestjs/common";
+import { LocationsService } from "src/modules/locations";
+import { parseOrderBy } from "src/utils";
 
 @Controller("locations")
-export class LocationsController {}
+export class LocationsController {
+    constructor(private readonly locationsService: LocationsService) {}
+
+    @Get()
+    async findAll(
+        @Query(
+            "skip",
+            new DefaultValuePipe(undefined),
+            new ParseIntPipe({ optional: true }),
+        )
+        skip: number,
+        @Query(
+            "take",
+            new DefaultValuePipe(10),
+            new ParseIntPipe({ optional: true }),
+        )
+        take: number,
+        @Query("orderBy", new DefaultValuePipe(undefined)) orderBy: string,
+    ) {
+        const result = await this.locationsService.getLocations({
+            skip: skip,
+            take: take,
+            orderBy: parseOrderBy(orderBy),
+        });
+
+        const meta = {
+            skip: skip,
+            take: take,
+            ...{
+                orderBy: orderBy ? (JSON.parse(orderBy) as unknown) : {},
+            },
+        };
+
+        return {
+            data: result,
+            meta: meta,
+        };
+    }
+}
--- src/modules/locations/locations.service.ts
+++ src/modules/locations/locations.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+import { PrismaService } from "src/databases";
+import { Location } from "src/modules/locations/entries";
 
 @Injectable()
-export class LocationsService {}
+export class LocationsService {
+    constructor(private prisma: PrismaService) {}
+
+    async getLocations(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.locationsWhereUniqueInput;
+        where?: Prisma.locationsWhereInput;
+        orderBy?: Prisma.locationsOrderByWithRelationInput;
+    }): Promise<Location[]> {
+        const { skip, take, cursor, where, orderBy } = params;
+
+        return this.prisma.locations.findMany({
+            skip: skip,
+            take: take,
+            cursor: cursor,
+            where: where,
+            orderBy: orderBy,
+        });
+    }
+}
